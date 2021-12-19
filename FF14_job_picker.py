1555be3fd8efbf0e8ce938e176f7a2a73a93c0ed
SA <62649689+scottyarn@users.noreply.github.com>
2021-12-19 17:52:17 +0000

    Update FF14_job_picker.py
    added two new classes to both the DPS and healer lists.

--- FF14_job_picker.py
+++ FF14_job_picker.py
@@ -5,8 +5,8 @@ import sys
 # creates a list of the current jobs in the game
 
 tank = ["Warrior","Paladin","Dark Knight","Gunbreaker"]
-heal = ["White Mage","Scholar","Astrologian"]
-dps = ["Monk","Dragoon","Ninja","Samurai","Bard","Machinist","dancer","Black-mage","Summoner","Red-Mage"]
+heal = ["White Mage","Scholar","Astrologian","Sage"]
+dps = ["Monk","Dragoon","Ninja","Samurai","Bard","Machinist","dancer","Black-mage","Summoner","Red-Mage","Reaper"]
 # This is for all the jobs in the game
 every_class = tank+dps+heal
 
